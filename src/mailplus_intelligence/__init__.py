24273a1337e21e6cca64f9cf79b49553a3e8c392
John McChesney TenEyck Jr <john.m.teneyck@gmail.com>
2026-05-01 15:01:05 -0500

    Add privacy-safe metadata fixtures

--- src/mailplus_intelligence/__init__.py
+++ src/mailplus_intelligence/__init__.py
@@ -1,10 +1,13 @@
 """MailPlus Intelligence runtime foundations."""
 
+from .fixtures import MetadataFixtureCorpus, load_metadata_fixture_corpus
 from .runtime import RuntimeProfile, default_runtime_profile
 from .sqlite import connect_sqlite
 
 __all__ = [
+    "MetadataFixtureCorpus",
     "RuntimeProfile",
     "connect_sqlite",
     "default_runtime_profile",
+    "load_metadata_fixture_corpus",
 ]
